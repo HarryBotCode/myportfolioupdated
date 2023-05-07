92fc57fe6f76b16ed1a036a75cc87152ac2db0cc
HarryBotCode <harisjavedawan123@gmail.com>
2023-05-07 17:22:45 +0500

    first commit

---
+++ src/constants/constants.js
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    title: 'Social Media App',
+    description: "A social media app that allows users to login and post interesting events and utilize the full functionalities of the application like creating new ID, Message, Comment, User Searching, Follow Friends etc.",
+      image: '/images/1.png',
+      tags: ['React.js', 'Node.js', 'MySql', 'Bootstrap'],
+    source: 'https://github.com/HarryBotCode/shopmanagement',
+    visit: 'https://socialmediaapp.netlify.app',
+    id: 0,
+  },
+  {
+    title: 'E-Commerce',
+    description:"An online supplement store that allows users to buy their favourite supplements. I've used many advanced React & JavaScript, and used Material-UI for the front-end to make a responsive design.",
+    image: '/images/2.png',
+    tags: ['React', 'Node', 'MongoDB', 'Material-UI'],
+    source: 'https://github.com/HarryBotCode/e-commerce',
+    visit: 'https://aestheticnutrition.netlify.app',
+    id: 1,
+  },
+  {
+    title: 'TOURING WEBSITE',
+    description: "A Touring Website that allows users to select beautiful locations for their vacation trips. Used advanced React, JavaScript and Chakra-UI for the front-end for creating the good responsive design.",
+      image: '/images/3.png',
+      tags: ['React', 'Javascript', 'Chakra-UI'],
+    source: 'https://github.com/HarryBotCode/turio',
+    visit: 'https://touring-website.netlify.app/',
+    id: 2,
+  },
+  {
+    title: 'MY PORTFOLIO',
+    description: "I have made my personal portfolio website that you are watching right now with advanced technologies like React.js and Next.js. I hope you like my work.",
+    image: '/images/4.png',
+    tags: ['React', 'Next', 'Express'],
+    source: 'https://github.com/HarryBotCode/portfolio-project',
+    visit: 'https://harrisjavedportfolio.netlify.app',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  
+  { year: 2021, text: 'Worked as a Freelancer', },
+  { year: 2022, text: 'Worked as a Front-End Developer at Softosol', },
+  { year: 2023, text: 'Working as a Front-End React Developer at B-Tech Groups', },
+];
